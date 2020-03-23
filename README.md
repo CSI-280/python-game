75a2ec87f8286d0bd857e4d655cb2af1c47c6b36
Michael Leonard <michael.leonard@mymail.champlain.edu>
2020-03-23 17:53:45 -0400

    Basic setup files and readme update for installation guide

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # python-game
 
+# Installation
 
-This is a placeholder
+You'll need to install libtcod to run this project.
+
+### On Windows (non virtual environment)
+
+Run the following code in a command-line to install tcod on your computer
+```
+py -m pip install tcod
+```
+
+### PyCharm (virtual environment)
+
+1. Upgrade pip using the command
+```
+python -m pip install --upgrade pip
+```
+
+2. Install tcod using the command
+```
+pip install tcod
+```
+
+If you need help, this tutorial from the library's page is very helpful and where I got the information on how to install it in the first place.
+
+https://python-tcod.readthedocs.io/en/latest/installation.html
---
+++ engine.py
@@ -0,0 +1,38 @@
+# this code is from http://rogueliketutorials.com/tutorials/tcod/part-1/
+
+import tcod as libtcod
+
+
+def main():
+    screen_width = 80
+    screen_height = 50
+
+    # set the font (arial10x10.png is in project folder, can be changed)
+    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+
+    # create the screen
+    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+
+    # game loop
+    while not libtcod.console_is_window_closed():
+        # set the color for the @ symbol
+        libtcod.console_set_default_foreground(0, libtcod.white)
+
+        # arg 1: console we're printing to
+        # arg 2: x coordinate
+        # arg 3: y coordinate
+        # arg 4: symbol to print
+        # arg 5: background (set to none)
+        libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE)
+        libtcod.console_flush()
+
+        # check for keyboard input
+        key = libtcod.console_check_for_keypress()
+
+        # if escape is pressed, quit game loop
+        if key.vk == libtcod.KEY_ESCAPE:
+            return True
+
+
+if __name__ == '__main__':
+    main()
